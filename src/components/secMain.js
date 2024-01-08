1be0c45e835500929fd34142696977b4b9142f5e
Ta-Run <tarnsharma11091999@gmail.com>
2024-01-08 14:28:17 +0530

    fetch registration api

--- src/components/secMain.js
+++ src/components/secMain.js
@@ -1,15 +1,20 @@
-import React, { useContext, useRef} from 'react'
+import React, { useContext, useRef } from 'react'
 import contextImg from './context/imgContext'
 
 function SecMain(props) {
+
   let inputRef = useRef(null);
+
   const contextImages = useContext(contextImg);
+
   const handleSearch = () => {
     inputRef.current.value = 'Iphone';
     inputRef.current.focus();
     inputRef.current.style.color = 'blue';
   }
+
   if (contextImages.loading) return <h1>loading</h1>
+
   return (
     <div>
       <p> After set state props will be change {props.mainState}</p>
@@ -18,20 +23,29 @@ function SecMain(props) {
         <button onClick={handleSearch}>Searach Images</button>
       </div>
       {contextImages &&
-        contextImages.imgs.map((img,index) => {
+        contextImages.imgs.map((img, index) => {
           // console.log(img.description)
           return (
-            <div style={{ display: 'flex' }} key={index}>
-              <div >
-                < img src={img.thumbnail}   lalt='natural' style={{ justifyContent: 'flex-start' }} />
-              </div>
-              <div style={{background:'yellow'}}>
-                <button>
-                  Buy
-                </button>
-                <p>{img.description}</p>
-              </div>
+            <div key={index} className='flex justify-center  bg-fuchsia-400' >
+
+                <div>
+                  <div>
+                    < img src={img.thumbnail} alt='natural' />
+                  < div className='box-border h-32 w-52 p-4 border-4 '>
+                    <p>{img.description.substring(0,15)}</p>
+               <button>
+               Buy
+             </button>
+             </div>
+                  </div>
+                </div>
+
+
+
+
+
             </div>
+
           )
         })
       }
